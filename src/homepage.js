0e741558b751641ad4c49ffa354db7153bd7c2de
Trey Morris <treymorrisit@hotmail.com>
2021-10-26 15:27:38 -0400

    finished laying out home page

--- src/homepage.js
+++ src/homepage.js
@@ -5,17 +5,17 @@ tabsContainer.classList.add('tabs-container');
 content.appendChild(tabsContainer);
 
 const tab1 = document.createElement('div');
-tab1.classList.add('tabs');
+tab1.classList.add('tab1');
 tabsContainer.appendChild(tab1);
 tab1.textContent = 'Home';
 
 const tab2 = document.createElement('div');
-tab2.classList.add('tabs');
+tab2.classList.add('tab2');
 tabsContainer.appendChild(tab2);
 tab2.textContent = 'Menu'
 
 const tab3 = document.createElement('div');
-tab3.classList.add('tabs');
+tab3.classList.add('tab3');
 tabsContainer.appendChild(tab3);
 tab3.textContent = 'Contact'
 
@@ -90,3 +90,20 @@ const footer = document.createElement('div');
 footer.classList.add('footer');
 content.appendChild(footer);
 
+const footerLink1 = document.createElement('a');
+footerLink1.setAttribute('href', "yourlink.here");
+footerLink1.classList.add('footer-link1');
+footer.appendChild(footerLink1);
+footerLink1.innerText = "click me now"
+
+const footerLink2 = document.createElement('a');
+footerLink2.setAttribute('href', "yourlink.here");
+footerLink2.classList.add('footer-link2');
+footer.appendChild(footerLink2);
+footerLink2.innerText = 'or click me';
+
+const footerLink3 = document.createElement('a');
+footerLink3.setAttribute('href', "yourlink.here");
+footerLink3.classList.add('footer-link3');
+footer.appendChild(footerLink3);
+footerLink3.innerText = 'or click me';
